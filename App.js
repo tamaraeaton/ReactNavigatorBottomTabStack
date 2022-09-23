18533a4dc91540fc248067ea80cb1d0d4d2cbf03
Tamara Eaton <Tamara.Eaton@bounteous.com>
2022-09-23 13:30:08 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView>
+      <Text>Tamara's Mobile App 1</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
